fd7456b5d9561364e527b5c0439899e37db3ab5f
yunext280 <yunext280@users.noreply.github.com>
2026-07-02 21:38:17 +0100

    v1.082: threaded=True, setInterval check, fix Stop and stream display

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ if __name__ == "__main__":
     sock.close()
     with open(os.path.expanduser("~/flask.port"), "w") as f:
         f.write(str(port))
-    app.run(host="127.0.0.1", port=port, use_reloader=False)
+    app.run(host="127.0.0.1", port=port, threaded=True, use_reloader=False)
